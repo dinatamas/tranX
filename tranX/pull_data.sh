85ee4b1d466d22411a7f2307cea3a279e9168329
Dina Tamás Pál <53911660+dinatamas@users.noreply.github.com>
2020-10-01 01:35:13 +0200

    Add our dataset to pull_data.sh

--- tranX/pull_data.sh
+++ tranX/pull_data.sh
@@ -2,11 +2,16 @@
 
 echo "download dataset"
 data_file="tranx.0.2.1.zip"
-wget -c http://www.cs.cmu.edu/~pengchey/${data_file}
+wget -c http://dinatamas.web.elte.hu/${data_file}
 unzip ${data_file}
 rm ${data_file}
 
-for dataset in django geo atis jobs wikisql conala;
+data_file="our.zip"
+wget -c http://dinatamas.web.elte.hu/${data_file}
+unzip ${data_file}
+rm ${data_file}
+
+for dataset in django geo atis jobs wikisql conala our;
 do
 	mkdir -p saved_models/${dataset}
 	mkdir -p logs/${dataset}
